b803e8ff37553734c0723cd66b2ff581f06df242
Anish Tiwari <anish.at.tiwari@gmail.com>
2024-03-02 12:31:56 +0530

    feat: environment setup + folder structuring

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,31 @@
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import '../globals.css';
+import { ClerkProvider } from '@clerk/nextjs';
+import { dark } from '@clerk/themes';
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+    title: 'Socio',
+    description: 'Join social media for stalkers',
+};
+
+export default function RootLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <ClerkProvider
+            appearance={{
+                baseTheme: dark,
+            }}
+        >
+            <html lang="en">
+                <body className={`${inter.className} bg-dark-1`}>
+                    {children}
+                </body>
+            </html>
+        </ClerkProvider>
+    );
+}
