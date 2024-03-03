8d13e15385644d62486203461f94d384e5dd8099
Anish Tiwari <anish.at.tiwari@gmail.com>
2024-03-03 23:41:21 +0530

    feat: added onboarding flow + uploadthing

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -1,7 +1,26 @@
+import AccountProfile from '@/components/forms/AccountProfile';
+import { currentUser } from '@clerk/nextjs';
 import React from 'react';
 
-const OnBoarding = () => {
-    return <div className="text-white">page</div>;
+const OnBoarding = async () => {
+    const user = await currentUser();
+    const userData = {
+        profile_photo: user?.imageUrl || '',
+        name: user?.firstName || '',
+        username: user?.username || '',
+    };
+    return (
+        <main className="mx-auto flex max-w-3xl flex-col justify-start px-10 py-20">
+            <h1 className="head-text">Onboarding</h1>
+            <p className="mt-3 text-base-regular text-light-2">
+                Complete your profile now, to use Socio.
+            </p>
+
+            <section className="mt-9 bg-dark-2 p-10">
+                <AccountProfile user={userData} btnTitle="Continue" />
+            </section>
+        </main>
+    );
 };
 
 export default OnBoarding;
